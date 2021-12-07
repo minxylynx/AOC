eb84e96bb6d64a87b67c4bfd29f90dd482e89e9f
Carolyn Russell <crussell@figure.com>
2021-12-06 23:04:16 -0700

    Day 7 solutions

--- src/year2021/day07/Day07.kt
+++ src/year2021/day07/Day07.kt
@@ -1,19 +1,31 @@
 package year2021.day07
 
 import readInput
+import splitToInt
+import kotlin.math.abs
+import kotlin.math.floor
+import kotlin.math.roundToInt
 
-/**
-  [link](https://adventofcode.com/2021/day/7)
-*/
 
+//  https://adventofcode.com/2021/day/7
+
+fun Array<Int>.fuelUse(endPoint: Int) =
+    this.sumOf { start ->
+        val diff = IntProgression.fromClosedRange(start, endPoint, 1.takeIf { start <= endPoint } ?: -1).count() - 1
+        IntRange(1, diff).sum()
+    }
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val array = input.splitToInt(",").flatten().toTypedArray().sortedArray()
+        val median = array[array.size / 2]
+        return array.sumOf { abs(it - median) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val array = input.splitToInt(",").flatten().toTypedArray().sortedArray()
+        val avg = array.average()
+        return minOf(array.fuelUse(floor(avg).toInt()), array.fuelUse(avg.roundToInt()))
     }
 
     val input = readInput(2021, "07")
